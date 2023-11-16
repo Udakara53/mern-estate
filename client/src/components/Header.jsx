204b24be49a214985954077f6d63267342033ab9
Udakara53 <udakaratharindu@gmail.com>
2023-11-16 18:43:38 +0530

    create user model

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,8 +11,8 @@ export default function Header() {
             <span className='text-slate-700'>Estate</span>
         </h1>
         </Link>
-        <form className='bg-slate-100 rounded-lg flex items-center'>
-            <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+        <form className='bg-slate-100 rounded-lg flex items-center  h-10 sm:h-12 p-2 sm:p-3'>
+            <input type="text" placeholder='Search.......' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
             <FaSearch className='text-slate-600 '/>
         </form>
         <ul className='flex gap-3'>
